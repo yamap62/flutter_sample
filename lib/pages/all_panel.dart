921624651e59598468f6755671ac811170b68ee3
Yuya Yamada <yuya_0602@icloud.com>
2021-09-11 01:32:45 +0900

    create fields

--- lib/pages/all_panel.dart
+++ lib/pages/all_panel.dart
@@ -1,77 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:sample_app/components/form_dialog.dart';
 import 'package:sample_app/components/panel.dart';
 
+import '../utils.dart';
+
 class AllPanelPage extends StatefulWidget {
-  AllPanelPage({Key? key}) : super(key: key);
+  final String title;
+  const AllPanelPage({Key? key, required this.title}) : super(key: key);
 
   @override
   _AllPanelPageState createState() => _AllPanelPageState();
 }
 
 class _AllPanelPageState extends State<AllPanelPage> {
+  static const countMatrix = 3;
+  static const double size = 92;
+  late List<List<Panel>> matrix;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Column(
-            children: [
-              Text(
-                'ナビゲーション',
-                style: Theme.of(context)
-                    .textTheme
-                    .subtitle2
-                    ?.copyWith(color: Colors.white),
-              ),
-            ],
-          ),
-        ),
-        body: Column(
+  void initState() {
+    super.initState();
+
+    setPanels();
+  }
+
+  void setPanels() => setState(() => matrix = List.generate(
+        countMatrix,
+        (_) => List.generate(countMatrix, (_) => Panel()),
+      ));
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Column(
           children: [
-            ...List.generate(
-              9,
-              (index) => Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Panel(text: 'test1'),
-                    Panel(text: 'test2'),
-                    Panel(text: 'test3'),
-                    Panel(text: 'test4'),
-                    Panel(text: 'test5'),
-                    Panel(text: 'test6'),
-                    Panel(text: 'test7'),
-                    Panel(text: 'test8'),
-                    Panel(text: 'test9'),
-                  ],
-                ),
-              ),
+            Text(
+              widget.title,
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle2
+                  ?.copyWith(color: Colors.white),
             ),
           ],
-        )
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: Utils.modelBuilder(matrix, (x, value) => buildRow(x)),
+      ));
 
-        // bottomNavigationBar: BottomNavigationBar(
-        //   currentIndex: _pageIndex,
-        //   onTap: (selectedIndex) {
-        //     setState(() {
-        //       _pageIndex = selectedIndex;
-        //     });
-        //   },
-        //   items: [
-        //     BottomNavigationBarItem(
-        //       icon: const Icon(Icons.photo_library),
-        //       label: localizations.demoFadeThroughAlbumsDestination,
-        //     ),
-        //     BottomNavigationBarItem(
-        //       icon: const Icon(Icons.photo),
-        //       label: localizations.demoFadeThroughPhotosDestination,
-        //     ),
-        //     BottomNavigationBarItem(
-        //       icon: const Icon(Icons.search),
-        //       label: localizations.demoFadeThroughSearchDestination,
-        //     ),
-        //   ],
-        // ),
-        );
+  Widget buildRow(int x) {
+    final values = matrix[x];
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: Utils.modelBuilder(
+        values,
+        (y, value) => buildField(x, y),
+      ),
+    );
+  }
+
+  Widget buildField(int x, int y) {
+    final value = matrix[x][y];
+
+    return Container(
+      margin: const EdgeInsets.all(4),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(size, size),
+        ),
+        child: Text(value.text, style: const TextStyle(fontSize: 32)),
+        onPressed: () => fillOutPanel(value.text, x, y),
+      ),
+    );
+  }
+
+  void fillOutPanel(String value, int x, int y) {
+    FormDialog.show(context);
   }
 }
